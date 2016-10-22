acbe0537e3b709a6e65a7db1ca2d4083e3348fc3
habib121 <habibr.rehman89@gmail.com>
2016-10-22 17:13:00 +0530

    first commit

---
+++ src/COM/HABEEB/aartek/HelloGit.java
@@ -0,0 +1,11 @@
+package COM.HABEEB.aartek;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("git sucess");
+	}
+
+}
